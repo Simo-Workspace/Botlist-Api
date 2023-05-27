7ecfa65dc53b8d704ee4dda8049fbc391980d86f
yUnreal <unrealwebdev@gmail.com>
2023-05-27 12:46:12 -0300

    Better types for src/constrollers/index.d.ts

--- src/controllers/index.d.ts
+++ src/controllers/index.d.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { ExpressPromise } from "../typings";
 
-declare const deleteBot = async (req: Request, res: Response) => ExpressPromise;
-declare const getBot = async (req: Request, res: Response) => ExpressPromise;
-declare const editBot = async (req: Request, res: Response) => ExpressPromise;
-declare const addBot = async (req: Request, res: Response) => ExpressPromise;
+declare const deleteBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
+declare const getBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
+declare const editBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
+declare const addBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
