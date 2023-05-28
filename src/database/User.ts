5bca989e5f856ada7802bdf39098ec502e0bff65
yUnreal <unrealwebdev@gmail.com>
2023-05-28 18:01:06 -0300

    New schema

---
+++ src/database/User.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+import type { UserStructure } from "../typings";
+
+const UserSchema = new Schema<UserStructure>({
+    _id: String,
+    username: String,
+    avatar: String,
+});
+
+export default model('User', UserSchema);
