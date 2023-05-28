ac13fe385a8f3e85dfd6e3c9cbe58dea541c9146
yUnreal <unrealwebdev@gmail.com>
2023-05-28 18:52:22 -0300

    Typing

--- app.d.ts
+++ app.d.ts
@@ -1,5 +1,7 @@
 import { Express } from "express";
+import { RateLimitRequestHandler } from "express-rate-limit";
 
 declare const app: Express;
+declare const limiter: RateLimitRequestHandler;
 
 export default app;
