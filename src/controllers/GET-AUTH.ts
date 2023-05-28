c8997434487959ceccb55a8a0102cf5fb7d25add
yUnreal <unrealwebdev@gmail.com>
2023-05-28 18:50:47 -0300

    New route

--- src/controllers/GET-AUTH.ts
+++ src/controllers/GET-AUTH.ts
@@ -1,45 +1,45 @@
-import axios from 'axios';
 import { Request, Response } from "express";
-import serialize from 'serialize-javascript';
+import { ExpressPromise } from "../typings";
+import serialize from "serialize-javascript";
+import { DiscordUserStructure } from "../typings";
+import { DISCORD_AUTH_ERROR } from "./errors.json";
+import { INTERNAL_SERVER_ERROR } from "./status-code.json";
 import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES } from "../../.config.json";
 
-export const callback = async (req: Request, res: Response) => {
-    const code = req.query.code;
-    const data: any = {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: REDIRECT_URI,
-      scope: SCOPES.join(' ')
-    };
-  
-    try {
-      const tokenResponse = await axios.post('https://discord.com/api/v10/oauth2/token', new URLSearchParams(data), {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      });
-  
-      const accessToken = tokenResponse.data.access_token;
-      const userResponse = await axios.get('https://discord.com/api/v10/users/@me', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      });
-  
-      const { username, id, avatar } = userResponse.data;
-      const user: any = {
-        username: username,
-        id: id,
-        avatar: avatar
+export const callback: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response): ExpressPromise => {
+  const code = req.query.code;
+  const data = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    grant_type: 'authorization_code',
+    code,
+    redirect_uri: REDIRECT_URI,
+    scope: SCOPES.join(' ')
+  };
+
+  try {
+    const req = await fetch('https://discord.com/api/v10/oauth2/token', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: new URLSearchParams(data as Record<string, string>) });
+    const tokenResponse = await req.json();
+
+    const accessToken: string = tokenResponse.data.access_token;
+    const request: globalThis.Response = await fetch('https://discord.com/api/v10/users/@me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
       }
-  
-      res.cookie('discordUser', serialize(user), { maxAge: 900000, httpOnly: false });
-  
-      res.redirect('http://localhost:5173');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Erro ao autenticar com o Discord');
-    }
+    });
+    const { username, id, avatar }: DiscordUserStructure = await request.json();
+    const user: DiscordUserStructure = {
+      username: username,
+      id: id,
+      avatar: avatar
+    };
+
+    res.cookie('discordUser', serialize(user), { maxAge: 900000, httpOnly: false });
+
+    res.redirect('http://localhost:5173');
+  } catch (error) {
+    console.error(error);
+
+    res.status(INTERNAL_SERVER_ERROR).send({ message: DISCORD_AUTH_ERROR, code: INTERNAL_SERVER_ERROR });
+  };
 };
