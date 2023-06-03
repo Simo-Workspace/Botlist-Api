bfc6a6c90e13331d799ea0f95cfdc856ffc7ce39
yUnreal <unrealwebdev@gmail.com>
2023-06-03 11:07:10 -0300

    Unnecessary .d.ts files

--- app.d.ts
+++
@@ -1,5 +0,0 @@
-import { Express } from "express";
-import { RateLimitRequestHandler } from "express-rate-limit";
-
-export declare const app: Express;
-export declare const limiter: RateLimitRequestHandler;
--- src/controllers/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { ExpressPromise } from "../typings";
-
-declare const deleteBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
-declare const getBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
-declare const editBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
-declare const addBot: (req: Request, res: Response) => ExpressPromise = async (req: Request, res: Response) => ExpressPromise;
