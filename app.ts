54c9ae8539699b56d507f4518e374ef32890e9f4
yUnreal <unrealwebdev@gmail.com>
2023-06-03 16:25:45 -0300

    Guild implementation

--- app.ts
+++ app.ts
@@ -14,8 +14,12 @@ import { DELETE } from "./src/controllers/bots/DELETE";
 import { callback } from "./src/controllers/bots/GET-AUTH";
 import { MANY_REQUEST } from "./src/controllers/errors.json";
 import { GET as GET_USER } from "./src/controllers/users/GET";
+import { GET as GET_GUILD } from "./src/controllers/guilds/GET";
+import { POST as POST_GUILD } from "./src/controllers/guilds/POST";
+import { PATCH as PATCH_GUILD } from "./src/controllers/guilds/PATCH";
 import { TOO_MANY_REQUESTS } from "./src/controllers/status-code.json";
 import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
+import { DELETE as DELETE_GUILD } from "./src/controllers/guilds/DELETE";
 
 const app: Express = express();
 
@@ -41,6 +45,7 @@ app.use(express.json({ strict: true, limit: "50kb" }), cors({ credentials: true 
 app.route(ROUTES.USER).get(GET_USER);
 app.route(ROUTES.AUTH).get(callback);
 app.route(ROUTES.MAIN).get(GET).delete(DELETE).patch(PATCH).post(POST).put(PUT);
+app.route(ROUTES.GUILD).get(GET_GUILD).delete(DELETE_GUILD).patch(PATCH_GUILD).post(POST_GUILD);
 
 app.listen(PORT, async (): Promise<void> => {
 	await connect(MONGOOSE_URL).catch(console.error);
