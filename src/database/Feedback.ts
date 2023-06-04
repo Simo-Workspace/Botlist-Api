245967a394cfba6c59d8d456dc48a34e879427e5
yUnreal <unrealwebdev@gmail.com>
2023-06-04 14:25:29 -0300

    New schema

---
+++ src/database/Feedback.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+import { MODELS_NAME } from "../../constants.json";
+import { FeedbackStructure } from "../typings/index";
+
+const FeedbackSchema = new Schema<FeedbackStructure>({
+	author: String,
+	stars: Number,
+	postedAt: String,
+	content: String,
+	targetBot: String
+});
+
+export default model(MODELS_NAME.Feedbacks, FeedbackSchema);
