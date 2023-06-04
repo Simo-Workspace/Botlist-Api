31398c7c10a4db908d14404ff6e1543e245e936c
yUnreal <unrealwebdev@gmail.com>
2023-06-04 17:53:49 -0300

    Implementing new methods in app

--- app.ts
+++ app.ts
@@ -19,35 +19,39 @@ import { PATCH as PATCH_GUILD } from "./src/controllers/guilds/PATCH";
 import { TOO_MANY_REQUESTS } from "./src/controllers/status-code.json";
 import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
 import { DELETE as DELETE_GUILD } from "./src/controllers/guilds/DELETE";
+import { POST as POST_FEEDBACK } from "./src/controllers/feedbacks/POST";
+import { PATCH as PATCH_FEEDBACK } from "./src/controllers/feedbacks/PATCH";
+import { DELETE as DELETE_FEEDBACK } from "./src/controllers/feedbacks/DELETE";
 
 const app: Express = express();
 
 const limiter: RateLimitRequestHandler = rateLimit({
-	max: 10,
-	message: {
-		error: MANY_REQUEST,
-		code: TOO_MANY_REQUESTS
-	},
-	statusCode: TOO_MANY_REQUESTS
+    max: 10,
+    message: {
+        error: MANY_REQUEST,
+        code: TOO_MANY_REQUESTS
+    },
+    statusCode: TOO_MANY_REQUESTS
 });
 
 app.use(express.json({ strict: true, limit: "50kb" }), cors({ credentials: true }), limiter, cookieParser(), session({
-	secret: COOKIE_SECRET,
-	cookie: {
-		maxAge: 24 * 60 * 60 * 1000 * 7,
-	},
-	resave: false,
-	saveUninitialized: false,
-	name: "discord.login"
+    secret: COOKIE_SECRET,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000 * 7,
+    },
+    resave: false,
+    saveUninitialized: false,
+    name: "discord.login"
 }));
 
 app.route(ROUTES.USER).get(GET_USER);
 app.route(ROUTES.AUTH).get(callback);
 app.route(ROUTES.MAIN).get(GET).delete(DELETE).patch(PATCH).post(POST);
+app.route(ROUTES.FEEDBACK).patch(PATCH_FEEDBACK).delete(DELETE_FEEDBACK).post(POST_FEEDBACK);
 app.route(ROUTES.GUILD).get(GET_GUILD).delete(DELETE_GUILD).patch(PATCH_GUILD).post(POST_GUILD);
 
 app.listen(PORT, async (): Promise<void> => {
-	await connect(MONGOOSE_URL).catch(console.error);
+    await connect(MONGOOSE_URL).catch(console.error);
     
-	console.info(`Servidor iniciado na porta ${PORT}`);
+    console.info(`Servidor iniciado na porta ${PORT}`);
 });
