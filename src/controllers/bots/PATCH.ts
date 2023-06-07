4d2ddd1cd9875d090f70b66d849215249a234645
yUnreal <unrealwebdev@gmail.com>
2023-06-07 19:32:46 -0300

    Unnecessary constant

--- src/controllers/bots/PATCH.ts
+++ src/controllers/bots/PATCH.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { AUTH } from "../../../.config.json";
 import { GENERICS, BOT } from "../errors.json";
 import { default as BotSchema } from "../../database/Bot";
-import type { BotStructure, ExpressResponse, Snowflake } from "../../types/types";
+import { ExpressResponse, Snowflake } from "../../types/types";
 import { UNAUTHORIZED, NOT_FOUND, INTERNAL_SERVER_ERROR, OK } from "../status-code.json";
 
 /** Edit a bot */
@@ -15,8 +15,7 @@ export const PATCH: (req: Request, res: Response) => ExpressResponse = async (re
 
     if (!exists) return res.status(NOT_FOUND).json({ message: BOT.BOT_NOT_FOUND, code: NOT_FOUND });
 
-    const bodyData: Partial<BotStructure> = req.body;
-    const updated = await BotSchema.findByIdAndUpdate({ _id }, { ...bodyData, _id }, { new: true });
+    const updated = await BotSchema.findByIdAndUpdate({ _id }, req.body, { new: true });
 
     if (!updated) return res.status(INTERNAL_SERVER_ERROR).json({ message: BOT.CANNOT_EDIT_THE_BOT, code: INTERNAL_SERVER_ERROR });
 
