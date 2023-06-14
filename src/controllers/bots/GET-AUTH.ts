0a76a6fc71a784c3fa0d1d863bcc547c00ea1812
yUnreal <unrealwebdev@gmail.com>
2023-06-14 14:05:24 -0300

    Code fix

--- src/controllers/bots/GET-AUTH.ts
+++ src/controllers/bots/GET-AUTH.ts
@@ -1,14 +1,14 @@
 import { GENERICS } from "../errors.json";
 import { Request, Response } from "express";
 import serialize from "serialize-javascript";
-import { ExpressResponse } from "../../types/types";
 import { DiscordUserStructure } from "../../types/types";
 import { INTERNAL_SERVER_ERROR } from "../status-code.json";
-import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK } from "../../../.config.json";
 
 /** Webiste callback */
 
-export const callback: (req: Request, res: Response) => ExpressResponse = async (req: Request, res: Response): ExpressResponse => {
+export const callback: (req: Request, res: Response) => Promise<void> = async (req: Request, res: Response): Promise<void> => {
+    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK }: NodeJS.ProcessEnv = process.env;
+
     const code = req.query.code;
     const data = {
         client_id: CLIENT_ID,
@@ -16,14 +16,14 @@ export const callback: (req: Request, res: Response) => ExpressResponse = async 
         grant_type: "authorization_code",
         code,
         redirect_uri: REDIRECT_URI,
-        scope: SCOPES.join(" ")
+        scope: JSON.parse(SCOPES as string).join(" ")
     };
 
     try {
         const req: globalThis.Response = await fetch("https://discord.com/api/v10/oauth2/token", { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: new URLSearchParams(data as Record<string, string>) });
         const response: { error: string; access_token: string; } = await req.json();
 
-        if (response.error === "invalid_grant") return res.redirect(AUTH_LINK);
+        if (response.error === "invalid_grant") return res.redirect(AUTH_LINK as string);
 
         const accessToken: string = response.access_token;
         const request: globalThis.Response = await fetch("https://discord.com/api/v10/users/@me", {
@@ -33,14 +33,14 @@ export const callback: (req: Request, res: Response) => ExpressResponse = async 
         });
         const { username, id, avatar }: DiscordUserStructure = await request.json();
         const user: DiscordUserStructure = {
-            username: username,
-            id: id,
-            avatar: avatar
+            username,
+            id,
+            avatar
         };
 
         res.cookie("discordUser", serialize(user), { maxAge: 24 * 60 * 60 * 1000 * 7, httpOnly: false });
 
-        res.redirect(REDIRECT_AUTH);
+        res.redirect(REDIRECT_AUTH as string);
     } catch (error: unknown) {
         console.error(error);
 
