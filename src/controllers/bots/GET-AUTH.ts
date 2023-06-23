df4c7d143dc4c025784f97d5a659a2d1a82ce003
yUnreal <unrealwebdev@gmail.com>
2023-06-23 15:45:28 -0300

    Spyei, why not use your brain and the eslint

--- src/controllers/bots/GET-AUTH.ts
+++ src/controllers/bots/GET-AUTH.ts
@@ -1,15 +1,15 @@
+import jwt from "jsonwebtoken";
 import { GENERICS } from "../errors.json";
 import { Request, Response } from "express";
-import jwt from 'jsonwebtoken';
+import { UNAUTHORIZED, OK } from "../status-code.json";
 import { DiscordUserStructure } from "../../types/types";
 import { INTERNAL_SERVER_ERROR } from "../status-code.json";
-import { UNAUTHORIZED, NOT_FOUND, OK, BAD_REQUEST } from "../status-code.json";
 
 /** Webiste callback */
 
 export const callback = async (req: Request, res: Response) => {
-    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK, JWT_SECRET, AUTH }: NodeJS.ProcessEnv = process.env;
     const { code } = req.query;
+    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK, JWT_SECRET, AUTH }: NodeJS.ProcessEnv = process.env;
 
     const data = {
         client_id: CLIENT_ID,
@@ -20,28 +20,29 @@ export const callback = async (req: Request, res: Response) => {
         scope: JSON.parse(SCOPES as string).join(" ")
     };
 
-    if(req.params.method === 'user') {
+    if (req.params.method === "user") {
         try {
             //if (req.headers.authorization !== AUTH) return res.status(UNAUTHORIZED).json({ message: GENERICS.INVALID_AUTH, code: UNAUTHORIZED });
-            const userData = jwt.verify(req.cookies.discordUser, JWT_SECRET as string)
+            const userData = jwt.verify(req.cookies.discordUser, JWT_SECRET as string);
+            
             return res.json(userData);
-        } catch(error: unknown) {
-            return res.send(JSON.stringify(error))
+        } catch (error: unknown) {
+            return res.send(JSON.stringify(error));
         }
     }
 
-    if(req.params.method === 'logout') {
+    if (req.params.method === "logout") {
         try {
             if (req.headers.authorization !== AUTH) return res.status(UNAUTHORIZED).json({ message: GENERICS.INVALID_AUTH, code: UNAUTHORIZED });
             res.clearCookie("discordUser");
-            return res.send({ message: 'Success!' })
+
+            return res.send({ message: GENERICS.SUCCESS, code: OK });
         } catch (error: unknown) {
-            return res.json({ message: 'Error', error });
+            return res.json({ message: GENERICS.DISCORD_AUTH_ERROR, code: INTERNAL_SERVER_ERROR });
         }
     }
 
     try {
-        
         const req: globalThis.Response = await fetch("https://discord.com/api/v10/oauth2/token", { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: new URLSearchParams(data as Record<string, string>) });
         const response: { error: string; access_token: string; } = await req.json();
 
@@ -54,17 +55,13 @@ export const callback = async (req: Request, res: Response) => {
             }
         });
         const { username, id, avatar }: DiscordUserStructure = await request.json();
-        const user: DiscordUserStructure = {
-            username,
-            id,
-            avatar
-        };
+        const sevenDays: 604800000 = 604800000 as const;
 
         const token = jwt.sign({
-            data: user
-          }, JWT_SECRET as string, { expiresIn: 24 * 60 * 60 * 1000 * 7 });
+            data: { username, id, avatar }
+        }, JWT_SECRET as string, { expiresIn: sevenDays });
 
-        res.cookie("discordUser", token, { maxAge: 24 * 60 * 60 * 1000 * 7, httpOnly: false });
+        res.cookie("discordUser", token, { maxAge: sevenDays, httpOnly: false });
 
         res.redirect(REDIRECT_AUTH as string);
     } catch (error: unknown) {
