89600f02913fb3212958eb2b19c2a41636b52f08
yUnreal <unrealwebdev@gmail.com>
2023-06-27 16:41:29 -0300

    Code fix (Spyei, usa o eslint krl) and typing it

--- src/controllers/bots/GET-AUTH.ts
+++ src/controllers/bots/GET-AUTH.ts
@@ -4,20 +4,20 @@ import { JwtPayload } from "jsonwebtoken";
 import { Request, Response } from "express";
 import { UNAUTHORIZED, OK } from "../status-code.json";
 import { INTERNAL_SERVER_ERROR } from "../status-code.json";
-import { DiscordUserStructure, ExpressResponse } from "../../types/types";
+import { APIScopes, DiscordUserStructure, ExpressResponse, Snowflake } from "../../types/types";
 
 /** Webiste callback */
 
 export const callback: (req: Request, res: Response) => void = async (req: Request, res: Response): Promise<ExpressResponse | void> => {
     const { code } = req.query;
-    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK, JWT_SECRET, AUTH, COOKIE_SECRET }: NodeJS.ProcessEnv = process.env;
+    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES, REDIRECT_AUTH, AUTH_LINK, JWT_SECRET, AUTH }: NodeJS.ProcessEnv = process.env;
 
-    const data = {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
+    const data: { client_id: Snowflake; client_secret: string; grant_type: string; code: unknown; redirect_uri: string; scope: APIScopes[]; } = {
+        client_id: CLIENT_ID as Snowflake,
+        client_secret: CLIENT_SECRET as string,
         grant_type: "authorization_code",
         code,
-        redirect_uri: REDIRECT_URI,
+        redirect_uri: REDIRECT_URI as string,
         scope: JSON.parse(SCOPES as string).join(" ")
     };
 
@@ -35,7 +35,7 @@ export const callback: (req: Request, res: Response) => void = async (req: Reque
     if (req.params.method === "logout") {
         try {
             if (req.headers.authorization !== AUTH) return res.status(UNAUTHORIZED).json({ message: GENERICS.INVALID_AUTH, code: UNAUTHORIZED });
-            
+
             res.clearCookie("discordUser");
 
             return res.status(OK).json({ message: GENERICS.SUCCESS, code: OK });
@@ -44,9 +44,9 @@ export const callback: (req: Request, res: Response) => void = async (req: Reque
         }
     }
 
-    if(req.params.method === "callback") {
+    if (req.params.method === "callback") {
         try {
-            const req: globalThis.Response = await fetch("https://discord.com/api/v10/oauth2/token", { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: new URLSearchParams(data as Record<string, string>) });
+            const req: globalThis.Response = await fetch("https://discord.com/api/v10/oauth2/token", { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: new URLSearchParams(data as unknown as Record<string, string>) });
             const response: { error: string; access_token: string; } = await req.json();
 
             if (response.error === "invalid_grant") return res.redirect(AUTH_LINK as string);
@@ -54,22 +54,20 @@ export const callback: (req: Request, res: Response) => void = async (req: Reque
             const accessToken: string = response.access_token;
             const request: globalThis.Response = await fetch("https://discord.com/api/v10/users/@me", {
                 headers: {
-                Authorization: `Bearer ${accessToken}`
+                    Authorization: `Bearer ${accessToken}`
                 }
             });
             const { username, id, avatar }: DiscordUserStructure = await request.json();
             const sevenDays: 604800000 = 604800000 as const;
 
             const token: string = jwt.sign({
-            data: { username, id, avatar }
+                data: { username, id, avatar }
             }, JWT_SECRET as string, { expiresIn: sevenDays });
 
             res.cookie("discordUser", token, { maxAge: sevenDays });
 
             res.redirect(REDIRECT_AUTH as string);
         } catch (error: unknown) {
-            console.error(error);
-
             res.status(INTERNAL_SERVER_ERROR).json({ message: GENERICS.DISCORD_AUTH_ERROR, code: INTERNAL_SERVER_ERROR });
         }
     }
