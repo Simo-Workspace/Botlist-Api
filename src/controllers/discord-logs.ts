c92a2443f8e25876c710df103c5b6499d46060c1
yUnreal <unrealwebdev@gmail.com>
2023-07-09 18:41:08 -0300

    feat(ID): Including ID to username when log something

--- src/controllers/discord-logs.ts
+++ src/controllers/discord-logs.ts
@@ -10,7 +10,7 @@ export const log: (req: Request, _res: Response, next: NextFunction) => Promise<
     const avatar: string = `https://cdn.discordapp.com/avatars/${payload.id}/${payload.avatar}.png` as const;
 
     // always returns an empty string
-    await axios.post(`https://discord.com/api/v10/webhooks/1125236689485435011/${WEBHOOK_TOKEN}`, { content: `eu usei a api na rota \`${req.path}\``, username: payload.username, avatar_url: avatar }, { headers: { Authorization: `Bot ${CLIENT_TOKEN}` } }).catch(console.error);
+    await axios.post(`https://discord.com/api/v10/webhooks/1125236689485435011/${WEBHOOK_TOKEN}`, { content: `eu usei a api na rota \`${req.path}\``, username: payload.username + `(${payload.id})`, avatar_url: avatar }, { headers: { Authorization: `Bot ${CLIENT_TOKEN}` } }).catch(console.error);
 
     next();
 };
