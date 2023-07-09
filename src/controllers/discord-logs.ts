85d4c587bfa469d2cd71f97452a4d59145d29024
yUnreal <unrealwebdev@gmail.com>
2023-07-09 13:11:38 -0300

    feat(log): event cooler

--- src/controllers/discord-logs.ts
+++ src/controllers/discord-logs.ts
@@ -10,7 +10,7 @@ export async function log(req: Request, _res: Response, next: NextFunction) {
     const avatar: string = `https://cdn.discordapp.com/avatars/${payload.id}/${payload.avatar}.png` as const;
 
     // always returns an empty string
-    await axios.post(`https://discord.com/api/v10/webhooks/1125236689485435011/${WEBHOOK_TOKEN}`, { content: "eu usei a api", username: payload.username, avatar_url: avatar }, { headers: { Authorization: `Bot ${CLIENT_TOKEN}` } }).catch(console.error);
+    await axios.post(`https://discord.com/api/v10/webhooks/1125236689485435011/${WEBHOOK_TOKEN}`, { content: `eu usei a api na rota \`${req.path}\``, username: payload.username, avatar_url: avatar }, { headers: { Authorization: `Bot ${CLIENT_TOKEN}` } }).catch(console.error);
 
     next();
 }
