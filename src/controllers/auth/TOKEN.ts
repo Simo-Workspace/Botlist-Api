5cb0f62b8545dd3a46bffca2a645372a4b941696
yUnreal <unrealwebdev@gmail.com>
2023-07-10 15:26:01 -0300

    BREAKING_CHANGE: Routers folder was renamede to 'routers'

--- src/controllers/auth/TOKEN.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import { SyncExpressResponse } from "../../types/types";
-
-export const getToken: (req: Request, res: Response) => SyncExpressResponse = (req: Request, res: Response): SyncExpressResponse => {
-    return res.json({ token: req.cookies.discordUser as string });
-};
