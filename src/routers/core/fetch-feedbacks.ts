64d0ec44deba5fcfbb20b405f63d90d5360d4eaa
yUnreal <unrealwebdev@gmail.com>
2023-07-10 15:28:49 -0300

    chore(typing): Typing function

--- src/routers/core/fetch-feedbacks.ts
+++ src/routers/core/fetch-feedbacks.ts
@@ -2,9 +2,9 @@ import { FEEDBACK } from "../errors.json";
 import { Request, Response } from "express";
 import FeedbackSchema from "../../schemas/Feedback";
 import { NOT_FOUND, OK } from "../status-code.json";
-import { FeedbackStructure, Schema } from "../../types/types";
+import { ExpressResponse, FeedbackStructure, Schema } from "../../types/types";
 
-export const fetchFeedbacks = async (req: Request, res: Response) => {
+export const fetchFeedbacks: (req: Request, res: Response) => ExpressResponse = async (req: Request, res: Response): ExpressResponse => {
     const { id: targetBot } = req.params;
 
     const data: Schema<FeedbackStructure>[] | null = await FeedbackSchema.find({ targetBot });
