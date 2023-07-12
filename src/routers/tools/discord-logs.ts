ac47217169ee4a4a1bc6e186acf6ad674fd55aba
yUnreal <unrealwebdev@gmail.com>
2023-07-12 13:58:39 -0300

    fix(NoJwtPayload): Wow, look at him? Is that no payload?

--- src/routers/tools/discord-logs.ts
+++ src/routers/tools/discord-logs.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
+import { BAD_REQUEST } from "./status-code.json";
 import { JwtPayload, verify } from "jsonwebtoken";
+import { ExpressResponse } from "../../core/types/types";
 import { NextFunction, Response, Request } from "express";
 
-export const log: (req: Request, _res: Response, next: NextFunction) => Promise<void> = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+export const log: (req: Request, _res: Response, next: NextFunction) => Promise<ExpressResponse | void> = async (req: Request, _res: Response, next: NextFunction): Promise<ExpressResponse | void> => {
     const { CLIENT_TOKEN, WEBHOOK_TOKEN }: NodeJS.ProcessEnv = process.env;
 
     const payload: JwtPayload = verify(req.headers.authorization as string, process.env.JWT_SECRET as string) as JwtPayload;
+
+    if (!payload.id) return _res.json({ message: "Wow, look at him. Withour JwtPatload is not it?", code: BAD_REQUEST });
 
     const avatar: string = `https://cdn.discordapp.com/avatars/${payload.id}/${payload.avatar}.png` as const;
 
